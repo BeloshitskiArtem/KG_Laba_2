0c15f50bb25e2535aa93e88f9b3a7682f870fbe4
Белошицкий Артём <38163250+BeloshitskiArtem@users.noreply.github.com>
2022-03-12 23:54:42 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,404 @@
+﻿namespace LINII
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(600, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(365, 535);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(334, 509);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберете алгоритм";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(214, 231);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 29);
+            this.button9.TabIndex = 20;
+            this.button9.Text = "Нарисовать";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(186, 420);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(139, 29);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "Окружность";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(186, 471);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 27);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "3 Вариант";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(14, 421);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(138, 27);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Треугольник";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(186, 375);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 27);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Квадрат";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 375);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 27);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Прямоугольник";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(234, 327);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(74, 27);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 327);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(203, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Шаг пунктира (от 10 до 200)";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 281);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 27);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Тонкая линия";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 281);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 27);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Толстая линия";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 235);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Цвет";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(74, 233);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(121, 27);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 179);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Yk";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 138);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Xk";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Yn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Xn";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(74, 177);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 27);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(74, 136);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 27);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 99);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 62);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 471);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 24);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(133, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Обычный ЦДА";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(547, 534);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 549);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Генератор отрезков";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        /*private Panel panel1;
+        private GroupBox groupBox1;
+        private Button button1;
+        private RadioButton radioButton1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private TextBox textBox5;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox6;
+        private Label label6;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;*/
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+//по списку
+//вариант 3
+
+namespace LINII
+{
+    public partial class Form1 : Form
+    {
+
+        private static void PutPixel(Graphics g, Color col, int x, int y, int alpha)
+        {
+            g.FillRectangle(new SolidBrush(Color.FromArgb(alpha, col)), x, y, 3, 3);
+        }
+
+        public double xn;
+        public double yn;
+        public double xk = 0;
+        public double yk = 0;
+
+        public int sizeLine = 2;
+
+        private string GetColor(int s1)
+        {
+            switch (s1)
+            {
+                case 1:
+                {
+                    return "Black";
+                    //break;
+                }
+                case 2:
+                    {
+                        return "Red";
+                        //break;
+                    }
+                case 3:
+                    {
+                        return "Blue";
+                        //break;
+                    }
+                case 4:
+                    {
+                        return "Green";
+                        //break;
+                    }
+                case 5:
+                    {
+                        return "Yellow";
+                        //break;
+                    }
+                case 6:
+                    {
+                        return "Purple";
+                        //break;
+                    }
+                default:
+                    {
+                        return "Purple";
+                        //break; 
+                    }
+            }
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                //это координаты пикселя, в который
+                //попал курсор при нажатии
+                xn = e.X;
+                yn = e.Y;
+            }
+
+            else MessageBox.Show("Вы не выбрали алгоритм вывода фигуры!");
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int n;
+            double xt, yt, dx, dy;
+            xk = e.X;
+            yk = e.Y;
+
+            //Приращение координат
+            dx = xk - xn;
+            dy = yk - yn;
+            n = int.Parse(textBox6.Text);  //пунктир  
+
+            //Занести начальную точку отрезка и нарисовать
+            xt = xn;
+            yt = yn;
+
+            for (int i = 0; i < n; i++)
+            {
+                //Объявляем объект "g" класса Graphics и предоставляем
+                //ему возможность рисования на pictureBox1
+                Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+
+                //Рисуем закрашенный прямоугольник:
+                //Объявляем объект "brush", задающий цвет кисти
+                int s1 = int.Parse(textBox5.Text);
+                Color c = (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(GetColor(s1));
+                SolidBrush brush = new SolidBrush(c);
+                //Рисуем закрашенный прямоугольник
+                g.FillRectangle(brush, (int)xt, (int)yt, sizeLine, sizeLine);
+                xt = xt + dx / n;
+                yt = yt + dy / n;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox5.Text = "1";
+            textBox6.Text = "100";
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            sizeLine = 5;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sizeLine = 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {     
+            int s1 = int.Parse(textBox5.Text);
+            Color c = (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(GetColor(s1));
+            Pen myPen = new Pen(c, sizeLine);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            //Рисуем прямоугольник:
+            g.DrawRectangle(myPen, 10, 20, 100, 200);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int s1 = int.Parse(textBox5.Text);
+            Color c = (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(GetColor(s1));
+            Pen myPen = new Pen(c, sizeLine);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            //Рисуем прямоугольник:
+            g.DrawRectangle(myPen, 10, 20, 100, 100);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int s1 = int.Parse(textBox5.Text);
+            Color c = (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(GetColor(s1));
+            Pen myPen = new Pen(c, sizeLine);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+
+            //первая линия, (x,y) для второй точки первой линии
+            g.DrawLine(myPen, 300, 100, 100, 600);
+            //вторая линия, (x,y) для второй точки второй линии
+            g.DrawLine(myPen, 300, 100, 300, 600);
+            //третья линия, (x,y) для второй точки третьей линии
+            g.DrawLine(myPen, 100, 600, 300, 600);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int s1 = int.Parse(textBox5.Text);
+            Color c = (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(GetColor(s1));
+            Pen myPen = new Pen(c, sizeLine);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+
+            //левая вертикальная линия
+            g.DrawLine(myPen, 100, 100, 200, 100);
+            //первая диагональная линия
+            g.DrawLine(myPen, 200, 100, 100, 500);
+            //нижняя горизонтальная линия
+            g.DrawLine(myPen, 100, 500, 270, 450);
+            //правая наклонная линия
+            g.DrawLine(myPen, 270, 450, 100, 100);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int s1 = int.Parse(textBox5.Text);
+            Color c = (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(GetColor(s1));
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            Pen myPen = new Pen(c, sizeLine);
+
+            BresenhamCircle(g, myPen, c, 100, 100, 50);
+        }
+
+        public static void BresenhamCircle(Graphics g, Pen myPen, Color clr, int _x, int _y, int radius)
+        {
+            int x = 0;
+            int y = radius;
+            int gap = 0;
+            int delta = (2 - 2 * radius);
+
+            while (y >= 0)
+            {
+                PutPixel(g, clr, _x + x, _y + y, 255);
+                PutPixel(g, clr, _x + x, _y - y, 255);
+                PutPixel(g, clr, _x - x, _y - y, 255);
+                PutPixel(g, clr, _x - x, _y + y, 255);
+                gap = 2 * (delta + y) - 1;
+                if (delta < 0 && gap <= 0)
+                {
+                    x++;
+                    delta += 2 * x + 1;
+                    continue;
+                }
+                if (delta > 0 && gap > 0)
+                {
+                    y--;
+                    delta -= 2 * y + 1;
+                    continue;
+                }
+                x++;
+                delta += 2 * (x - y);
+                y--;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            double xt = int.Parse(textBox1.Text);
+            double yt = int.Parse(textBox2.Text);
+            double dx = int.Parse(textBox3.Text);
+            double dy = int.Parse(textBox4.Text);
+
+            int s1 = int.Parse(textBox5.Text);
+            Color c = (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(GetColor(s1));
+            Pen myPen = new Pen(c, sizeLine);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            //Рисуем отрезок:
+            g.DrawLine(myPen, (int)xt, (int)yt, (int)dx, (int)dy);
+        }
+    }
+}
